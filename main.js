d5401c6f4e9f9017f96eb09a1204bd1d215eeae1
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-03-11 10:09:03 -0500

    first push

--- main.js
+++ main.js
@@ -1,33 +1,57 @@
 // ┌─────────────────┐
-// │ Your code here! │	
+// │ Your code here! │
 // └─────────────────┘
+function isAdmin (str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].userRole === "ADMIN") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return str.userRole
+}
 
+getEmail = (str) => {
 
+}
+
+getPlaylistLength = (str) => {
+
+}
+
+getHardestHomework = (str) => {
+
+}
+
+createPhonebook = (str) => {
+
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
 
-if (typeof isAdmin === 'undefined') {
-    isAdmin = undefined;
+if (typeof isAdmin === "undefined") {
+  isAdmin = undefined;
 }
-if (typeof getEmail === 'undefined') {
-    getEmail = undefined;
+if (typeof getEmail === "undefined") {
+  getEmail = undefined;
 }
-if (typeof getPlaylistLength === 'undefined') {
-    getPlaylistLength = undefined;
+if (typeof getPlaylistLength === "undefined") {
+  getPlaylistLength = undefined;
 }
-if (typeof getHardestHomework === 'undefined') {
-    getHardestHomework = undefined;
+if (typeof getHardestHomework === "undefined") {
+  getHardestHomework = undefined;
 }
-if (typeof createPhonebook === 'undefined') {
-    createPhonebook = undefined;
+if (typeof createPhonebook === "undefined") {
+  createPhonebook = undefined;
 }
 
 module.exports = {
-    isAdmin,
-    getEmail,
-    getPlaylistLength,
-    getHardestHomework,
-    createPhonebook
+  isAdmin,
+  getEmail,
+  getPlaylistLength,
+  getHardestHomework,
+  createPhonebook,
 };
