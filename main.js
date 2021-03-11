245f59261643cd2a084a301f7402cc681df92c7f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-03-11 12:09:36 -0500

    final solution

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 // ┌─────────────────┐
 // │ Your code here! │
 // └─────────────────┘
-isAdmin = (user) => {
-    if (user.userRole === "ADMIN") {
-        return true;
-    } else {
-        return false;
-    }
-}
-// => return codeimmersives email 
-getEmail = (object) => {
-    console.log(object.firstName);
-    console.log(object.lastName);
-    return object.firstName.toLowerCase() + "." + object.lastName.toLowerCase() + "@codeimmersives.com"
-}
+// isAdmin = (user) => {
+//     if (user.userRole === "ADMIN") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// // => return codeimmersives email 
+// getEmail = (object) => {
+//     console.log(object.firstName);
+//     console.log(object.lastName);
+//     return object.firstName.toLowerCase() + "." + object.lastName.toLowerCase() + "@codeimmersives.com"
+// }
 
-getPlaylistLength = (playlistObject) => {
-    return playlistObject.songs.length;
-}
+// getPlaylistLength = (playlistObject) => {
+//     return playlistObject.songs.length;
+// }
 
-getHardestHomework = (homeworks) => {
-    console.log(homeworks);
-    let lowestAverageScore = Infinity;
-    let nameOfHardest = '';
+// getHardestHomework = (homeworks) => {
+//     console.log(homeworks);
+//     let lowestAverageScore = Infinity;
+//     let nameOfHardest = '';
 
-    for (const item of homeworks) {
-        // console.log(item);
-        // console.log(item.name);
-        // console.log(item.averageScore);
-        if (item.averageScore < lowestAverageScore) {
-            nameOfHardest = item.name;
-            lowestAverageScore = item.averageScore
-        }
-    }
-    return nameOfHardest;
-}
+//     for (const item of homeworks) {
+//         // console.log(item);
+//         // console.log(item.name);
+//         // console.log(item.averageScore);
+//         if (item.averageScore < lowestAverageScore) {
+//             nameOfHardest = item.name;
+//             lowestAverageScore = item.averageScore
+//         }
+//     }
+//     return nameOfHardest;
+// }
 
 // function getHardestHomework (grade) {
 //     let hardestName = "";
@@ -48,25 +48,15 @@ getHardestHomework = (homeworks) => {
 //     return hardestName;
 // }
 
-function getHardestHomework (arr) {
-    let homework = ''
-    let num = []
-        for (const work of arr){
-            num.push(work.averageScore)
-        } 
-        const i = num.indexOf(Math.min.apply(null,num))
-        if (arr[i]!==undefined){
-        homework = arr[i].name
-        } else {
-            homework = ''
-        }
-    return homework
-}
+createPhonebook = (names, numbers) => {
+    console.log(names);
+    console.log(numbers);
+    const phonebook = {};
 
-
-
-createPhonebook = (str) => {
-
+    for (let i = 0; i < names.length; i++) {
+        phonebook[names[i]] = numbers[i];
+    }
+    return phonebook;
 }
 
 // ┌─────────────────────────────────────┐
